0beed86255145694d2f7b47a79f55a4a88160400
Paul Miller <paul@paulmillr.com>
2022-01-18 01:24:43 +0200

    Fix packages

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { pbkdf2, pbkdf2Async } from '@noble/hashes/pbkdf2';
 import { sha256 } from '@noble/hashes/sha256';
 import { sha512 } from '@noble/hashes/sha512';
 import { assertBytes, assertNumber, randomBytes } from '@noble/hashes/utils';
-import { chain, checksum, radix2, alphabet } from '@scure/base';
+import { utils as baseUtils } from '@scure/base';
 
 // Japanese wordlist
 const isJapanese = (wordlist: string[]) => wordlist[0] === '\u3042\u3044\u3053\u304f\u3057\u3093';
@@ -38,7 +38,11 @@ function getCoder(wordlist: string[]) {
   wordlist.forEach((i) => {
     if (typeof i !== 'string') throw new Error(`Wordlist: non-string element: ${i}`);
   });
-  return chain(checksum(1, calcChecksum), radix2(11, true), alphabet(wordlist));
+  return baseUtils.chain(
+    baseUtils.checksum(1, calcChecksum),
+    baseUtils.radix2(11, true),
+    baseUtils.alphabet(wordlist)
+  );
 }
 
 export function mnemonicToEntropy(mnemonic: string, wordlist: string[]): Uint8Array {
