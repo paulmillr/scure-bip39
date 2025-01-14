482ee055289faa51cdfc82aa2689d50cb975c76b
Paul Miller <paul@paulmillr.com>
2025-01-14 04:02:06 +0000

    test: commit js files

---
+++ test/assert.js
@@ -0,0 +1,72 @@
+// Minimal assert version to avoid dependencies on node internals
+// Allows to verify that none of browserify version of node internals is included in resulting build
+function deepStrictEqual(actual, expected, message) {
+    const [actualType, expectedType] = [typeof actual, typeof expected];
+    const err = new Error(`Non-equal values: actual=${actual} (type=${actualType}) expected=${expected} (type=${expectedType})${message ? `. Message: ${message}` : ''}`);
+    if (actualType !== expectedType) {
+        throw err;
+    }
+    // Primitive types
+    if (['string', 'number', 'bigint', 'undefined', 'boolean'].includes(actualType)) {
+        if (actual !== expected) {
+            throw err;
+        }
+        return;
+    }
+    if (actual instanceof Uint8Array && expected instanceof Uint8Array) {
+        if (actual.length !== expected.length) {
+            throw err;
+        }
+        for (let i = 0; i < actual.length; i++) {
+            if (actual[i] !== expected[i]) {
+                throw err;
+            }
+        }
+        return;
+    }
+    if (Array.isArray(actual) && Array.isArray(expected)) {
+        if (actual.length !== expected.length) {
+            throw err;
+        }
+        for (let i = 0; i < actual.length; i++) {
+            deepStrictEqual(actual[i], expected[i], message);
+        }
+        return;
+    }
+    if (actual === null && expected === null) {
+        return;
+    }
+    if (actualType === 'object') {
+        const [actualKeys, expectedKeys] = [
+            Object.keys(actual),
+            Object.keys(expected),
+        ];
+        deepStrictEqual(actualKeys, expectedKeys, message);
+        for (const key of actualKeys) {
+            deepStrictEqual(actual[key], expected[key], message);
+        }
+        return;
+    }
+    throw err;
+}
+async function throws(cb) {
+    try {
+        cb();
+    }
+    catch (e) {
+        return;
+    }
+    throw new Error('Missing expected exception.');
+}
+async function rejects(cb) {
+    try {
+        await cb();
+    }
+    catch (e) {
+        return;
+    }
+    throw new Error('Missing expected rejection.');
+}
+// Run tests with node assert:
+// import { deepStrictEqual, throws } from "assert";
+export { deepStrictEqual, throws, rejects };
---
+++ test/index.js
@@ -0,0 +1,3 @@
+import { it } from 'micro-should';
+import './bip39.test.js';
+it.runWhen(import.meta.url);
